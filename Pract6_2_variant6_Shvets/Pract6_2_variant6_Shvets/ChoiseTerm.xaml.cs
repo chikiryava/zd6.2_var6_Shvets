f64f3ceda940af4eb1028c05df854d6b58c19547
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-06-22 12:56:20 +0500

    Add files via upload

---
+++ Pract6_2_variant6_Shvets/Pract6_2_variant6_Shvets/ChoiseTerm.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pract6_2_variant6_Shvets
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChoiseTerm :ContentPage
+    {
+        public ChoiseTerm (string typeOfWork,string mark,int maintenence)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pract6_2_variant6_Shvets/Pract6_2_variant6_Shvets/CountSumm.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pract6_2_variant6_Shvets
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CountSumm :ContentPage
+    {
+        public CountSumm ()
+        {
+            InitializeComponent();
+        }
+    }
+}
